7d979b3b4bac77998925d86b9d8497a2d156897d
Harry Phillips <harry.phillips@connect.qut.edu.au>
2021-02-28 21:40:22 +1000

    Move directly from the helmet

--- rename.py
+++ rename.py
@@ -1,12 +1,13 @@
 import os
+import shutil
 
-main_path   = "D:/vids/raw"
-output_path = "D:/vids/forcite"
+helmet_path = "F:/Forcite"
+video_loc   = "D:/vids/forcite"
 
-directories = os.listdir( main_path )
+directories = os.listdir( helmet_path )
 
-for sub_folder in os.listdir(main_path):
-    if os.path.isdir(os.path.join(main_path, sub_folder)):
+for sub_folder in os.listdir(helmet_path):
+    if os.path.isdir(os.path.join(helmet_path, sub_folder)):
         #print(sub_folder)
         year   = sub_folder[:4]+'-'
         month  = sub_folder[4:6]+'-'
@@ -14,16 +15,16 @@ for sub_folder in os.listdir(main_path):
         hour   = sub_folder[8:10]+'.'
         minute = sub_folder[10:12]+'.'
         #print ( year+month+day+hour+minute )
-        sub_path = main_path + "/"+sub_folder
+        sub_path = helmet_path + "/"+sub_folder
         videos = os.listdir( sub_path )
         videos.sort()
         for file in videos:
             end_bit = file[-10:] 
             #print(file, end_bit)
             source      = sub_path + "/" + file
-            destination = output_path + "/" + year+month+day+hour+minute+end_bit
+            destination = video_loc + "/" + year+month+day+hour+minute+end_bit
             #print (source, destination)
-            os.rename(source, destination)
-        os.rmdir(main_path+'/'+sub_folder)
+            shutil.move(source, destination)
+        os.rmdir(helmet_path+'/'+sub_folder)
 
         
