ba786b16a33176c7bac5a22630958822593f05bf
Harry Phillips <harry@tux.com.au>
2021-09-13 14:37:29 +1000

    Working app - V1

--- FileTransfer/FileTransfer/Program.cs
+++ FileTransfer/FileTransfer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileTransfer
 {
@@ -6,7 +7,59 @@ namespace FileTransfer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] drives = System.IO.Directory.GetLogicalDrives();
+            string curDir = Environment.CurrentDirectory;
+            string movieFileName;
+            string movieFullName;
+            string movieNewName;
+            string yR;
+            string mN;
+            string dD;
+            string hR;
+            string mI;
+            string sC;
+
+            foreach (string drive in drives)
+            {
+                if (drive != "C:\\")
+                {
+                    string forcite = drive + "forcite";
+                    // System.Console.WriteLine(drive);
+                    if (Directory.Exists (forcite) )
+                    {
+                        Console.WriteLine($"The directory {forcite} exists");
+                        Console.WriteLine();
+                        
+                        DirectoryInfo forciteDir = new DirectoryInfo(forcite);
+                        DirectoryInfo[] myMoviesDir = forciteDir.GetDirectories();
+                        // myDirs
+                        foreach ( DirectoryInfo dir in myMoviesDir)
+                        {
+                            //Console.WriteLine(dir);
+                            FileInfo[] movies = dir.GetFiles();
+                            foreach (FileInfo movie in movies)
+                            {
+                                //Console.WriteLine(movie);
+                                movieFileName = movie.Name;
+                                movieFullName = movie.FullName;
+                                yR = movieFileName.Substring(0, 4);
+                                mN = movieFileName.Substring(4, 2);
+                                dD = movieFileName.Substring(6, 2);
+                                hR = movieFileName.Substring(8, 2);
+                                mI = movieFileName.Substring(10, 2);
+                                sC = movieFileName.Substring(12, 6);
+                                movieNewName = curDir + "\\" + yR + "-" + mN + "-" + dD + "_" + hR + "." + mI + "." + sC + ".mp4";
+                                Console.WriteLine(movieFullName + " -> " + movieNewName);
+                                File.Copy(movieFullName, movieNewName, true);
+                                //;
+                            }
+
+
+                        }
+                    }
+                }
+            }
+            // Console.ReadLine();
         }
     }
 }
